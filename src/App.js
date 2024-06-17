d6b6d2b1bab6c50b0a19e51b9de07aab34f08dcc
VIKRAM SINGH <bobvik2003@gmail.com>
2024-06-17 23:14:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Introduction from './components/Introduction/Introduction ';
+
+function App() {
+  return (
+    <div className="App">
+   <Introduction/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Introduction/Introduction .jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Introduction .css"
+const Introduction  = () => {
+  return (
+    <div className="introduction-container">
+    <h1>Hello! I'm Vikram Singh</h1>
+    <p>
+      I'm a passionate MERN Full Stack Developer with a knack for building dynamic and responsive web applications. 
+      With a strong foundation in MongoDB, Express.js, React, and Node.js, I enjoy creating seamless user experiences and efficient back-end solutions.
+    </p>
+    <p>
+      My expertise spans across front-end development, back-end integration, and database management, enabling me to deliver full-fledged web solutions.
+    </p>
+    <p>
+      Let's connect and build something amazing together!
+    </p>
+  </div>
+  )
+}
+
+export default Introduction;
