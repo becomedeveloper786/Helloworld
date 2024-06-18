5571d8cd661f1c090770c043dfbbfd72808b72c6
VIKRAM SINGH <bobvik2003@gmail.com>
2024-06-18 12:45:37 +0530

    first commit

---
+++ src/components/DynamicData/DynamicData.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DynamicData = () => {
+    const Name="Vikram Singh";
+  return (
+    <div>
+      {Name}
+    </div>
+  )
+}
+
+export default DynamicData;
--- src/components/Introduction/Introduction .jsx
+++ src/components/Introduction/Introduction .jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./Introduction .css"
+import DynamicData from '../DynamicData/DynamicData';
+
 const Introduction  = () => {
   return (
     <div className="introduction-container">
-    <h1>Hello! I'm Vikram Singh</h1>
+    <h1>Hello! I'm <DynamicData/></h1>
+  
     <p>
       I'm a passionate MERN Full Stack Developer with a knack for building dynamic and responsive web applications. 
       With a strong foundation in MongoDB, Express.js, React, and Node.js, I enjoy creating seamless user experiences and efficient back-end solutions.
